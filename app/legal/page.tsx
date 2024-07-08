1d52597e2cd6c2f09480f494ca6a8ec5752a62b9
Peter Strøiman <peter@stroiman.com>
2024-07-08 16:32:10 +0200

    Stuff

---
+++ app/legal/page.tsx
@@ -0,0 +1,16 @@
+export default function Legal() {
+  return (<>
+    <h1>Company Information</h1>
+    <p>
+      I operate from a Danish registered company.
+
+      Peter Strøiman ApS
+      <span id='company-registration-no-desc'>
+        Danish company registration no.:
+      </span> <span aria-describedBy='company-registration-no-desc'>32290671</span>
+
+    </p>
+
+  </>
+  )
+}
