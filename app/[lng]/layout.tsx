c2996380151683d7395f333eec45650cd974d37d
Peter Strøiman <peter@stroiman.com>
2024-07-13 13:28:09 +0200

    feat: Localize opengraph properties

--- app/[lng]/layout.tsx
+++ app/[lng]/layout.tsx
@@ -4,7 +4,7 @@ import { Inter, Source_Serif_4 } from "next/font/google";
 import "../globals.css";
 import { clsx } from "clsx";
 import { languages } from "../i18n/settings";
-import { Lang } from "../i18n";
+import { Lang, LangParam } from "../i18n";
 
 const inter = Inter({ subsets: ["latin"] });
 const sourceSefif = Source_Serif_4({
@@ -16,34 +16,79 @@ export async function generateStaticParams() {
   return languages.map((lng) => ({ lng }));
 }
 
-export const metadata: Metadata = {
-  title: "Peter Strøiman - Freelance Software Developer",
-  description: "Freelance software udvikler med mere end 25 års erfaring",
-  openGraph: {
-    title: "Peter Strøiman",
-    description: "Freelance Software Udvikler",
-    url: "https://stroiman.com/da",
-    siteName: "stroiman.com",
-    locale: "da-DK",
-    type: "website",
-  },
-  icons: {
-    icon: [
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-    ],
-    //shortcut: ["/shortcut-icon.png"],
-    apple: [
-      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
-    ],
-    //other: [
-    //  {
-    //    rel: "apple-touch-icon-precomposed",
-    //    url: "/apple-touch-icon-precomposed.png",
-    //  },
-    //],
-  },
-};
+export async function generateMetadata({ params }: { params: LangParam }) {
+  switch (params.lng) {
+    case "en":
+      return {
+        title: "Peter Strøiman - Freelance Software Developer",
+        description:
+          "Freelance software developer with 25+ years of experience",
+        openGraph: {
+          title: "Peter Strøiman",
+          description:
+            "Freelance software developer with 25+ years of experience",
+          url: "https://stroiman.com/en/",
+          siteName: "stroiman.com",
+          locale: "en-UK",
+          type: "website",
+        },
+        icons: {
+          icon: [
+            { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+            { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+          ],
+          //shortcut: ["/shortcut-icon.png"],
+          apple: [
+            {
+              url: "/apple-touch-icon.png",
+              sizes: "180x180",
+              type: "image/png",
+            },
+          ],
+          //other: [
+          //  {
+          //    rel: "apple-touch-icon-precomposed",
+          //    url: "/apple-touch-icon-precomposed.png",
+          //  },
+          //],
+        },
+      };
+    default:
+      return {
+        title: "Peter Strøiman - Freelance Software Developer",
+        description: "Freelance software udvikler med mere end 25 års erfaring",
+        openGraph: {
+          title: "Peter Strøiman",
+          description:
+            "Freelance software udvikler med mere end 25 års erfaring",
+          url: "https://stroiman.com/da/",
+          siteName: "stroiman.com",
+          locale: "da-DK",
+          type: "website",
+        },
+        icons: {
+          icon: [
+            { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+            { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+          ],
+          //shortcut: ["/shortcut-icon.png"],
+          apple: [
+            {
+              url: "/apple-touch-icon.png",
+              sizes: "180x180",
+              type: "image/png",
+            },
+          ],
+          //other: [
+          //  {
+          //    rel: "apple-touch-icon-precomposed",
+          //    url: "/apple-touch-icon-precomposed.png",
+          //  },
+          //],
+        },
+      };
+  }
+}
 
 export default async function RootLayout({
   children,
