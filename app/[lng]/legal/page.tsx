d357aff7941b9da1fa42c34c69b364c9b6fe7215
Peter Strøiman <peter@stroiman.com>
2024-07-13 10:29:07 +0200

    Fix aria-describedby

--- app/[lng]/legal/page.tsx
+++ app/[lng]/legal/page.tsx
@@ -1,16 +1,14 @@
 export default function Legal() {
-  return (<>
-    <h1>Company Information</h1>
-    <p>
-      I operate from a Danish registered company.
-
-      Peter Strøiman ApS
-      <span id='company-registration-no-desc'>
-        Danish company registration no.:
-      </span> <span aria-describedBy='company-registration-no-desc'>32290671</span>
-
-    </p>
-
-  </>
-  )
+  return (
+    <>
+      <h1>Company Information</h1>
+      <p>
+        I operate from a Danish registered company. Peter Strøiman ApS
+        <span id="company-registration-no-desc">
+          Danish company registration no.:
+        </span>{" "}
+        <span aria-describedby="company-registration-no-desc">32290671</span>
+      </p>
+    </>
+  );
 }
