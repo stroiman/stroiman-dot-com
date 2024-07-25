c78333637dbc97c08171c4fd75c4afb9269d91c7
Peter Strøiman <peter@stroiman.com>
2024-07-25 11:24:42 +0200

    Extract hamburger button

--- app/ui/menu.tsx
+++ app/ui/menu.tsx
@@ -6,6 +6,7 @@ import { ReactNode, useState } from "react";
 import { Logo } from "./image-svgs";
 import { Lang } from "../i18n";
 import Links from "../[lng]/links";
+import HamburgerButton from "./hamburger-button";
 
 export default function Menu({
   lng,
@@ -37,21 +38,8 @@ export default function Menu({
               stroiman.com
             </Link>
             <div>
-              <button
-                id="mobile-open"
-                className={clsx(
-                  "relative h-8 w-8 cursor-pointer text-3xl focus:outline-none md:hidden",
-                  {
-                    "[&>div::after]:translate-y-0 [&>div::after]:-rotate-45 [&>div::before]:translate-y-0 [&>div::before]:rotate-45 [&>div]:rotate-[360deg] [&>div]:bg-transparent":
-                      showMenu,
-                  },
-                )}
-                onClick={onClick}
-                // &#9776;
-              >
-                <div className="absolute top-4 -mt-0.5 h-1 w-8 rounded bg-white transition-all before:absolute before:h-1 before:w-8 before:-translate-x-4 before:-translate-y-2.5 before:rounded before:bg-white before:transition-all before:content-[''] after:absolute after:h-1 after:w-8 after:-translate-x-4 after:translate-y-2.5 after:rounded after:bg-white after:transition-all after:content-['']"></div>
-              </button>
-              {socialLinks}
+              <HamburgerButton onClick={onClick} showMenu={showMenu} />
+              <span className="hidden md:inline">{socialLinks}</span>
             </div>
           </section>
           <div className="flex flex-row">
