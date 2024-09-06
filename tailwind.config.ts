06e55497684758b6b5a522818dd67f0431bef63f
Peter Strøiman <peter@stroiman.com>
2024-09-06 12:04:10 +0200

    Invalid configuration generated by chadcn init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,55 +4,101 @@ import type { Config } from "tailwindcss";
 import colors from "tailwindcss/colors";
 
 const config: Config = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        //heading: ['"Source Serif 4"', "Georgia", "serif"],
-        heading: ["var(--source-serif)", "Georgia", "serif"],
-        body: ["Synonym", "system-ui", "sans-serif"],
-      },
-      spacing: {
-        target: "6rem",
-      },
-      colors: {
-        primary: colors.fuchsia,
-        secondary: colors.zinc,
-        body: colors.zinc,
-        em: colors.indigo[700],
-        "em-dark": colors.indigo[300],
-        muted: colors.neutral[500],
-      },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-      gridTemplateColumns: {
-        menu: "auto 1fr",
-      },
-      //scroll: {
-      //  normal: "scroll-mt-24",
-      //},
-      screens: {
-        widescreen: { raw: "(min-aspect-ratio: 3/2)" },
-        tallscreen: { raw: "(max-aspect-ratio: 13/20)" },
-      },
-      keyframes: {
-        "open-menu": {
-          "0%": { transform: "scaleY(0)" },
-          "100%": { transform: "scaleY(1)" },
-        },
-      },
-      animation: {
-        "open-menu": "open-menu 0.25s ease-in-out forwards",
-      },
-    },
+  	extend: {
+  		fontFamily: {
+  			heading: ["var(--source-serif)", "Georgia", "serif"],
+  			body: ["Synonym", "system-ui", "sans-serif"]
+  		},
+  		spacing: {
+  			target: '6rem'
+  		},
+  		colors: {
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			body: 'colors.zinc',
+  			em: 'colors.indigo[700],
+  			'em-dark': 'colors.indigo[300],
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		backgroundImage: {
+  			'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+  			'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
+  		},
+  		gridTemplateColumns: {
+  			menu: 'auto 1fr'
+  		},
+  		screens: {
+  			widescreen: {
+  				raw: '(min-aspect-ratio: 3/2)'
+  			},
+  			tallscreen: {
+  				raw: '(max-aspect-ratio: 13/20)'
+  			}
+  		},
+  		keyframes: {
+  			'open-menu': {
+  				'0%': {
+  					transform: 'scaleY(0)'
+  				},
+  				'100%': {
+  					transform: 'scaleY(1)'
+  				}
+  			}
+  		},
+  		animation: {
+  			'open-menu': 'open-menu 0.25s ease-in-out forwards'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
